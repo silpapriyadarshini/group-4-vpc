621f7e11bc1a01698009d29d63ce682882ceddf2
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-08-03 18:35:36 +0530

    added listener

--- alb/alb.tf
+++ alb/alb.tf
@@ -1,17 +1,24 @@
+#1. add subnet id
+
 resource "aws_lb" "alb_project" {
   name               = "alb-project"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [for subnet in aws_subnet.public : subnet.id]
+  #subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = true
 
-  access_logs {
-    bucket  = aws_s3_bucket.lb_logs.bucket
-    prefix  = "test-lb"
-    enabled = true
-  }
-
   tags = {
-    Environment = "production"
+    Name = "alb-project"
+  }
+}
+
+resource "aws_alb_listener" "listener_http" {
+  load_balancer_arn = aws_lb.alb_project.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.front_end.arn
   }
 }
