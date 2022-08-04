c23fd0e56e46ce90154ff07c4b6ad955ed54ebb0
Yerraguntla Satyakama Dattatreya <satya.kama@cloudreach.com>
2022-08-04 15:25:17 +0530

    changes to the reigon

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
     backend "s3" {
         bucket = "talent-academy-shoaib-lab-tfstates"
         key = "talent-academy/grp4_vpc/terraform.tfstates"
-        region = "ap-south-1"
+        region = "eu-west-1"
         dynamodb_table = "terraform-lock"
     }
 }
--- route.tf
+++ route.tf
@@ -35,7 +35,7 @@ resource "aws_route_table_association" "public_2" {
 }
 
 resource "aws_route_table_association" "private_1" {
-  subnet_id      = aws_subnet.private.id
+  subnet_id      = aws_subnet.private_1.id
   route_table_id = aws_route_table.nat_route_tbl.id
 }
 
