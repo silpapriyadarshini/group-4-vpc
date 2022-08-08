202b925ed26cea77eba2f64e31069823c38e526a
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-08-08 15:21:59 +0530

    removed backend and provider

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "talent-academy-shoaib-lab-tfstates"
-        key = "talent-academy/bastion-host/terraform.tfstates"
-        region = "eu-west-1"
-        dynamodb_table = "terraform-lock"
-    }
-}
